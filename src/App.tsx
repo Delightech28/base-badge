e37001c38ed0f5e22bd1f1e3eab120ec464a290e
Delightech28 <desolomon07@gmail.com>
2025-07-02 18:58:04 +0100

    jdjdj

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { useEffect } from "react";
 import { sdk } from "@farcaster/frame-sdk";
 import Welcome from "./components/Welcome";
-import Navbar from "./components/Navbar"; // <- new
-import "./index.css"; // <- for background styling
+import Navbar from "./components/Navbar";
+import "./index.css";
 import { useTokenBalance } from "./hooks/useTokenBalance";
 import Dashboard from "./components/Dashboard";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useConnect } from "wagmi";
 
 function App() {
   const { hasBalance } = useTokenBalance();
+  const { connect, connectors } = useConnect();
+
+  const handleConnect = () => {
+    connect({ connector: connectors[0] });
+  };
 
   useEffect(() => {
     sdk.actions.ready();
@@ -23,7 +29,7 @@ function App() {
       </div>
       <Navbar />
       <Routes>
-        <Route path="/" element={hasBalance ? <Dashboard /> : <Welcome />} />
+        <Route path="/" element={hasBalance ? <Dashboard /> : <Welcome onConnect={handleConnect} />} />
         <Route path="/profile" element={<Dashboard initialTab="profile" />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
