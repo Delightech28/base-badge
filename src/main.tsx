21d0466dce95de053b43132d7992772632c0c806
Delightech28 <desolomon07@gmail.com>
2025-12-05 07:56:07 +0100

    ngng

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,8 @@
+import { Buffer } from "buffer";
+if (!(globalThis as any).Buffer) {
+  (globalThis as any).Buffer = Buffer;
+}
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
